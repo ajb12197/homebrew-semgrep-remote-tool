f749bcd301da180e880ba0546dc64e25d4a2054c
AJ Butt <ajbutt@kali>
2025-08-29 11:57:30 -0400

    Setup up brew tap

---
+++ Formula/semgrep-remote-tool.rb
@@ -0,0 +1,22 @@
+class Semgrep-remote-tool < Formula
+    desc "Automated Semgrep update and rules checking"
+    homepage "https://github.com/ajbutt/"
+    url https://github.com//ajbutt/semgrep-remote-tool/releases/download/v12.19.7/semgrep-remote-tool-12.19.7.tar.gz"
+    sha256 "ca7a73aa046d83e9ada2aaa1b0e3eead6c902c9eff4b630aee44cd3449f2e555"
+    version "12.19.7"
+
+    def install
+        bin.install Dir["*"]
+        Dir["#{bin}/*"].each do |f|
+        system "codesign", "--force", "--sign", "-", f if File.file?(f)
+        end
+      end
+
+    def caveats
+        <<~EOS
+        To finish installation, add the following line to .zshrc or .bashrc  
+  source #{bin}/completion.sh
+Then reopen your terminal app
+        EOS
+    end
+    end
